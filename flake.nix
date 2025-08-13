991e86db9ad735ffa35401d9c43806c3400a1a6b
Lautaro Emanuel <emlautarom1@gmail.com>
2025-08-13 15:36:56 -0300

    Add kurtosis CLI

--- flake.nix
+++ flake.nix
@@ -2,16 +2,20 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
 
-      kurtosis = pkgs.stdenv.mkDerivation {
+      systems = {
+        x86_64-linux = "linux_amd64";
+      };
+
+      kurtosis = pkgs.stdenv.mkDerivation rec {
         pname = "kurtosis-cli";
         version = "1.10.3";
         src = pkgs.fetchurl {
-          url = "https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/1.10.3/kurtosis-cli_1.10.3_linux_amd64.tar.gz";
-          sha256 = "replace-with-actual-sha256";
+          url = "https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/${version}/kurtosis-cli_${version}_${systems.${system}}.tar.gz";
+          sha256 = "sha256-yDR7RuoSnQ4tZgNI5mAp/9ycZwaLxaZAbniqsiF7gxk=";
         };
         installPhase = ''
           mkdir -p $out/bin
@@ -21,7 +25,8 @@
     in
     {
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
+        buildInputs = [
+          kurtosis
         ];
       };
     }
