8844ae43b3652e75ad7335a5d171be4e8bea6149
Lautaro Emanuel <emlautarom1@gmail.com>
2025-10-30 12:09:22 -0300

    Bump kurtosis version

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
 
       kurtosis = pkgs.stdenv.mkDerivation rec {
         pname = "kurtosis-cli";
-        version = "1.10.3";
+        version = "1.11.2";
         src = pkgs.fetchurl {
           url = "https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/${version}/kurtosis-cli_${version}_${systems.${system}}.tar.gz";
-          sha256 = "sha256-yDR7RuoSnQ4tZgNI5mAp/9ycZwaLxaZAbniqsiF7gxk=";
+          sha256 = "sha256-LOBM9ZmmX6xbzLk4y0/a1/p0Kf3c/uZhxZC0VYEXqq4=";
         };
         installPhase = ''
           mkdir -p $out/bin
